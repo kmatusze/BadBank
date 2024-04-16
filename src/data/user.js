0a65bd9b6222805c7276d32df71a4a9aac22c580
kmatusze <138835328+kmatusze@users.noreply.github.com>
2024-04-15 20:15:17 -0400

    Add files via upload

---
+++ src/data/user.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    userPic: "/images/Avatar0.png",
+    name: "Abel",
+    email: "abel@mit.edu",
+    password: "secret",
+    balance: 1000,
+    transactionHistory: [],
+  },
+  {
+    userPic: "/images/Avatar0.png",
+    name: "Hope",
+    email: "hope@mit.edu",
+    password: "secret",
+    balance: 10000,
+    transactionHistory: [],
+  },
+];
+
+export default users;
